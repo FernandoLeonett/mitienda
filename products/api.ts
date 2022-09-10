3807b14c1668bf6748b961ba20e17372252a49bc
Fernando <fernandoleonett@gmail.com>
2022-09-09 22:52:19 -0300

    comented api excel

--- products/api.ts
+++ products/api.ts
@@ -1,35 +1,34 @@
-import axios from "axios";
-import Papa from "papaparse";
+import Product from "interfaces/Product";
 
-import Product from "../interfaces/Product";
-import {responseUrl} from "../utils/helper";
 
 
 export default {
   list: async (): Promise<Product[]> => {
-    return axios
-      .get(process.env.URL_EXCEL, {
-        responseType: "blob",
-      })
-      .then(
-        (response) =>
-          new Promise<Product[]>((resolve, reject) => {
-            Papa.parse(response.data, {
-              header: true,
-              complete: (results) => {
-                const products = results.data as Product[];
+    return[]
 
-                return resolve(
-                  products.map((product) => ({
-                    ...product,
-                    price: Number(product.price),
-                    image: responseUrl(product.image),
-                  })),
-                );
-              },
-              error: (error) => reject(error.message),
-            });
-          }),
-      );
+    // axios
+    //   .get(process.env.URL_EXCEL, {
+    //     responseType: "blob",
+    //   })
+    //   .then(
+    //     (response) =>
+    //       new Promise<Product[]>((resolve, reject) => {
+    //         Papa.parse(response.data, {
+    //           header: true,
+    //           complete: (results) => {
+    //             const products = results.data as Product[];
+
+    //             // return resolve(
+    //             //   products.map((product) => ({
+    //             //     ...product,
+    //             //     price: Number(product.price),
+    //             //     image: responseUrl(product.image),
+    //             //   })),
+    //             // );
+    //           },
+    //           error: (error) => reject(error.message),
+    //         });
+    //       }),
+    //   );
   },
 };
